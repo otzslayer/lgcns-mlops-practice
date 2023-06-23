d429277e29ff7eb64b315ff1b1318b2b7ced9ef3
Jay Han <otzslayer@gmail.com>
2023-06-23 06:07:50 +0000

    Update batch_prediction.sh

--- batch_prediction.sh
+++ batch_prediction.sh
@@ -1,9 +1,7 @@
 BASH_ENV=~/.bashrc
-ROOT_PATH=/workspaces/mlops-practice
+ROOT_PATH=/workspaces/lgcns-mlops-practice
 PIPENV_PIPFILE=$ROOT_PATH/Pipfile
-PYENV_VERSION=3.9.16
 
-export PATH=$PATH:/home/codespace/.pyenv/shims
+export PATH=$PATH:/usr/local/py-utils/bin
 export PIPENV_PIPFILE=$PIPENV_PIPFILE
-export PYENV_VERSION=$PYENV_VERSION
 pipenv run python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1
