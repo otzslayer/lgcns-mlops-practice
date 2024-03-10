504e5f0d6a04d6d221a428d08f1cffd7a76d8581
Jaeyoon Han <otzslayer@gmail.com>
2024-03-11 00:48:42 +0900

    Update files

--- batch_prediction.sh
+++ batch_prediction.sh
@@ -1,7 +1,4 @@
 BASH_ENV=~/.bashrc
 ROOT_PATH=/workspaces/lgcns-mlops-practice
-PIPENV_PIPFILE=$ROOT_PATH/Pipfile
 
-export PATH=$PATH:/usr/local/py-utils/bin
-export PIPENV_PIPFILE=$PIPENV_PIPFILE
-pipenv run python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1
+$ROOT_PATH/.venv/bin/python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1
